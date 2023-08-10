764b69fb53f059c5197a2e0f3c5b2d63ccccd6f6
O-01 <sa.an-s@yandex.com>
2023-08-10 13:59:20 -0500

    Added add - no errors upon testing with provided instruction file

--- add.c
+++ add.c
@@ -1,12 +1,25 @@
 #include "monty.h"
 
 /**
- * add - addition of top 2 elements in stack
+ * add - add top 2 node values in stack, second node takes sum, then pop top
  * @stack: stack, top 2 elements of which to be added
  * @line_n: line number within monty instruction file
  */
 
 void add(stack_t **stack, size_t line_n)
 {
-	(void)stack, (void)line_n;
+	stack_t *oldtopADD = NULL, *newtopSUM = NULL;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%lu: can't add, stack too short\n", line_n);
+		freeStack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	oldtopADD = *stack;
+	newtopSUM = oldtopADD->next;
+	newtopSUM->n += oldtopADD->n;
+
+	pop(stack, line_n);
 }
